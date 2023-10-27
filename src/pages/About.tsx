748eba1e833125842b35aacffd69d7bd8417f707
andre-vg <avictorg@gmail.com>
2023-10-27 20:14:08 -0300

    About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody } from '@nextui-org/react';
+import { Card, CardBody, CardHeader } from '@nextui-org/react';
 import { commonColors, semanticColors } from '@nextui-org/theme';
 import {
   VerticalTimeline,
@@ -7,6 +7,7 @@ import {
 import 'react-vertical-timeline-component/style.min.css';
 import TimelineBox from '../components/TimelineBox';
 import { FaGraduationCap } from 'react-icons/fa';
+import { MdOutlineWork } from 'react-icons/md';
 import { useState } from 'react';
 
 function About() {
@@ -16,22 +17,51 @@ function About() {
       <VerticalTimeline>
         <TimelineBox
           iconStyle={{
-            background: isDark ? semanticColors.dark.secondary[400]: semanticColors.light.primary[400],
+            background: isDark
+              ? semanticColors.dark.secondary[400]
+              : semanticColors.light.primary[400],
             color: '#fff',
           }}
           icon={<FaGraduationCap />}
           date="2018-2023"
         >
           <Card>
-            <CardBody>asdasdsad</CardBody>
+            <CardHeader className="p-4 text-right text-2xl">
+              Education
+            </CardHeader>
+            <p className="px-5 pb-5 font-medium">
+              Graduated from{' '}
+              <strong className="cursor-pointer text-secondary-400 hover:underline">
+                Unified Teaching Center of Brasília
+              </strong>{' '}
+              with a Bachelor of Science in Computer Science. Proficient in
+              Python and JavaScript. Experience in web development and project
+              management.
+            </p>
           </Card>
         </TimelineBox>
-        <TimelineBox date="2020-2222">
+        <TimelineBox
+          iconStyle={{
+            background: isDark
+              ? semanticColors.dark.secondary[400]
+              : semanticColors.light.primary[400],
+            color: '#fff',
+          }}
+          icon={<MdOutlineWork />}
+          date="2020-2222"
+        >
           <Card>
-            <CardBody>aaaaaa2</CardBody>
+            <CardHeader className="p-4 text-right text-2xl">
+              Experience
+            </CardHeader>
+            <CardBody className="font-medium">Pouca Experiencia </CardBody>
           </Card>
         </TimelineBox>
-        <VerticalTimelineElement date="2018-2023"></VerticalTimelineElement>
+        <TimelineBox date="2222-0000">
+          <Card>
+            <CardBody>carlos</CardBody>
+          </Card>
+        </TimelineBox>
         <VerticalTimelineElement date="2018-2023"></VerticalTimelineElement>
       </VerticalTimeline>
     </div>
