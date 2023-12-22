ccb26177f67b9888b837474a045e8f839d2fb244
andre-vg <avictorg@gmail.com>
2023-12-21 23:27:02 -0300

    ♻️ Refactor contact form and add new components

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,9 @@
-import { Button, Divider, Input, Textarea } from '@nextui-org/react';
+import { Button, Input, Textarea } from '@nextui-org/react';
 import { useTheme } from 'next-themes';
 import React, { useState } from 'react';
 import Translator from '../i18n/Translator';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
+import Footer from '../components/Footer';
+import { FaCheckCircle } from 'react-icons/fa';
 
 function Contact() {
   const { theme } = useTheme();
@@ -12,14 +12,36 @@ function Contact() {
     name: '',
     email: '',
     message: '',
+    _subject: 'Novo contato!!',
+    _captcha: 'false',
   });
+
+  const [loading, setLoading] = useState(false);
+  const [done, setDone] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  const handleClick = () => {
+    setLoading(true);
+    fetch('https://formsubmit.co/dd81f325ffc39e91ac747c3a7370dac7', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(form),
+    })
+      .then(() => {
+        setLoading(false);
+        setDone(true);
+      })
+      .catch(err => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-40 bg-[#dbdbdb] dark:bg-[#2c2d2d]">
+    <div className="flex h-screen flex-col items-center justify-evenly gap-24 bg-[#dbdbdb] dark:bg-[#2c2d2d] xl:justify-end xl:gap-48">
       <div className="sm:flex-row md:px-40 lg:gap-40 lg:px-80">
         <h1 className="text-center text-7xl font-bold text-[#2c2d2d] dark:text-[#dbdbdb]">
           <Translator path="contact.title" />
@@ -29,61 +51,64 @@ function Contact() {
         </p>
       </div>
       <form
-        className="flex w-3/4 flex-col gap-8 lg:w-1/2 xl:w-1/3 "
+        className="flex w-full flex-col items-center justify-center gap-8"
         action="https://formsubmit.co/dd81f325ffc39e91ac747c3a7370dac7"
         method="POST"
       >
-        <Input
-          onChange={e => handleChange(e)}
-          label="Nome"
-          type="text"
-          name="name"
-          required
-        />
-        <Input
-          label="Email"
-          type="email"
-          name="email"
-          onChange={e => handleChange(e)}
-          required
-        />
-        <input type="hidden" name="_subject" value="Novo contato!!" />
-        <input type="hidden" name="_captcha" value="false" />
-        <Textarea
-          label="Mensagem"
-          placeholder="Escreva sua mensagem..."
-          name="message"
-          onChange={e => handleChange(e)}
-          required
-        />
-        <Button
-          variant="shadow"
-          color={theme == 'light' ? 'primary' : 'secondary'}
-          isDisabled={!!!form.name || !!!form.email || !!!form.message}
-          type="submit"
+        <div
+          data-done={done}
+          className="absolute z-10 hidden h-96 w-3/4 flex-col items-center gap-16 p-8 transition-all duration-500 data-[done=true]:flex lg:w-1/2 xl:w-1/3"
         >
-          Enviar
-        </Button>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <Divider />
-          <div className="flex gap-4">
-            <a
-              href="https://www.linkedin.com/in/andr%C3%A9-gon%C3%A7alves-a74535163/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaLinkedin className="cursor-pointer text-4xl text-[#2c2d2d] transition-colors duration-300 hover:text-[#0a66c2] dark:text-[#dbdbdb] dark:hover:text-[#0a66c2]" />
-            </a>
-            <a
-              href="https://twitter.com/dedez00"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaXTwitter className="cursor-pointer text-4xl text-[#2c2d2d] transition-colors duration-300 hover:text-black dark:text-[#dbdbdb] dark:hover:text-white" />
-            </a>
+          {/* Obrigado pelo envio! */}
+          <FaCheckCircle className="text-9xl text-[#2c2d2d] dark:text-[#dbdbdb]" />
+          <div>
+            <h1 className="text-center text-4xl font-bold text-[#2c2d2d] dark:text-[#dbdbdb]">
+              <Translator path="contact.done.title" />
+            </h1>
+            <h3 className="text-center text-xl font-bold text-[#2c2d2d] dark:text-[#dbdbdb]">
+              <Translator path="contact.done.text" />
+            </h3>
           </div>
         </div>
+        <div
+          className="flex h-96 w-3/4 flex-col gap-8 transition-all duration-500 data-[done=true]:blur-sm lg:w-1/2 xl:w-1/3"
+          data-done={done}
+        >
+          <Input
+            onChange={e => handleChange(e)}
+            label="Nome"
+            type="text"
+            name="name"
+            required
+          />
+          <Input
+            label="Email"
+            type="email"
+            name="email"
+            onChange={e => handleChange(e)}
+            required
+          />
+          <input type="hidden" name="_subject" value="Novo contato!!" />
+          <input type="hidden" name="_captcha" value="false" />
+          <Textarea
+            label="Mensagem"
+            placeholder="Escreva sua mensagem..."
+            name="message"
+            onChange={e => handleChange(e)}
+            required
+          />
+          <Button
+            variant="shadow"
+            isLoading={loading}
+            color={theme == 'light' ? 'primary' : 'secondary'}
+            isDisabled={!!!form.name || !!!form.email || !!!form.message}
+            onClick={handleClick}
+          >
+            Enviar
+          </Button>
+        </div>
       </form>
+      <Footer />
     </div>
   );
 }
