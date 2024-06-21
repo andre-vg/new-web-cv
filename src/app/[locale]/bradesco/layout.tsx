9edf8b586e4d7dbe3135cb8a1f575ee7ff9181f2
André Gonçalves <andre.goncalves@grupoicts.com.br>
2024-06-21 14:31:40 -0300

    Ryb tests

---
+++ src/app/[locale]/bradesco/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function TimLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+        <Script
+          type="text/javascript"
+          src="https://cdn.rybena.com.br/dom/master/latest/rybena.js?player=AvatarBradesco"
+          ></Script>
+      <div>
+        AAAA
+        {children}
+      </div>
+    </>
+  );
+}
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,48 +1,48 @@
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "./providers";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
-import Navbar from "@/src/components/Navbar";
+import '@/styles/globals.css';
+import { Metadata } from 'next';
+import { siteConfig } from '@/config/site';
+import { fontSans } from '@/config/fonts';
+import { Providers } from './providers';
+import { Link } from '@nextui-org/link';
+import clsx from 'clsx';
+import Navbar from '@/src/components/Navbar';
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
-	children,
-	params: {locale}
+  children,
+  params: { locale },
 }: {
-	children: React.ReactNode;
-	params: {locale: string};
+  children: React.ReactNode;
+  params: { locale: string };
 }) {
-	return (
-		<html lang={locale} suppressHydrationWarning>
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}
-			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
-						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-							{children}
-						</main>
-						{/* <footer className="w-full flex items-center justify-center py-3">
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <head></head>
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+          <div className="relative flex h-screen flex-col">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl flex-grow px-6 pt-16">
+              {children}
+            </main>
+            {/* <footer className="w-full flex items-center justify-center py-3">
 							<Link
 								isExternal
 								className="flex items-center gap-1 text-current"
@@ -53,9 +53,9 @@ export default function RootLayout({
 								<p className="text-primary">NextUI</p>
 							</Link>
 						</footer> */}
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
