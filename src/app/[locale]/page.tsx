4149e758600f1fd3d6d489a1cc9241c717f0099d
André Gonçalves <andre.goncalves@grupoicts.com.br>
2024-07-31 18:56:40 -0300

    Ryb tests

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,9 @@
-import { useTranslations } from 'next-intl';
+import { Button } from '@nextui-org/button';
+import { useLocale, useTranslations } from 'next-intl';
 const Typer = dynamic(() => import('@/src/components/Home/typer'));
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
 export default function Home() {
   const t = useTranslations('hero');
   const phases = [t('adj.1'), t('adj.2'), t('adj.3'), t('adj.4'), t('adj.5')];
@@ -8,6 +11,9 @@ export default function Home() {
     <section>
       {/* @ts-ignore */}
       <Typer phrases={phases} />
+      <Link href={'/tim'}>
+        <Button>TIM</Button>
+      </Link>
     </section>
   );
 }
---
+++ src/app/[locale]/tim/page.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Script from 'next/script';
+import React from 'react';
+
+export default function Page() {
+  return (
+    <>
+      <div>OPA</div>
+    </>
+  );
+}
