9b1bda6d3b5b37b0a4117fe82a7d7b1808626701
andre-vg <avictorg@gmail.com>
2025-05-12 19:06:13 -0300

    🎉 Begin Refactor

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const createNextIntlPlugin = require('next-intl/plugin');
- 
-const withNextIntl = createNextIntlPlugin();
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
-module.exports = withNextIntl(nextConfig);
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {};
+
+export default config;
